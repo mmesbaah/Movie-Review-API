1ea30bde09beb3b63f6ada51503422fc4268f3fa
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-04-06 05:53:31 +0200

    updated

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -38,4 +38,4 @@ class ReviewSerializer(serializers.ModelSerializer):
     def validate_rating(self, value):
         if not 1 <= value <= 5:
             raise serializers.ValidationError("Rating must be between 1 and 5")
-        return value 
+        return value
