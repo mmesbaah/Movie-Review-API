28c3e7a35cbf4cf01fc8e02cb374cfab7adae3f6
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-04-06 05:53:07 +0200

    updated

--- movie_review_api/settings.py
+++ movie_review_api/settings.py
@@ -16,18 +16,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-development')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # Set to False to disable debug error page
+DEBUG = False  
 
 ALLOWED_HOSTS = ['*']  # Allow all hosts for development
 
